de2e83c1ae425485479efc8a94197e321f9ffe43
Alvaro-Kothe <kothe65@gmail.com>
2025-01-27 17:40:48 -0300

    fix: don't insert duplicate entries

--- dags/sql/insert_earthquake.sql
+++ dags/sql/insert_earthquake.sql
@@ -1,4 +1,15 @@
-INSERT INTO earthquake.earthquake (earthquake_id, position, depth, magnitude, time, alert, significance) VALUES 
+CREATE TEMP TABLE temp_earthquake_staging (
+  earthquake_id STRING,
+  position GEOGRAPHY,
+  depth FLOAT64,
+  magnitude FLOAT64,
+  time TIMESTAMP,
+  alert STRING,
+  significance INT64
+);
+
+INSERT INTO temp_earthquake_staging (earthquake_id, position, depth, magnitude, time, alert, significance)
+VALUES 
   {% for feature in task_instance.xcom_pull(task_ids='transform_features', key='return_value') %}
     (
     '{{ feature.earthquake_id }}',
@@ -8,6 +19,13 @@ INSERT INTO earthquake.earthquake (earthquake_id, position, depth, magnitude, ti
     '{{ feature.time }}',
     {{ feature.alert }},
     {{ feature.significance }}
-  ) {{ ", " if not loop.last else "" }}
+    ) {{ ", " if not loop.last else "" }}
   {% endfor %}
 ;
+
+MERGE INTO earthquake.earthquake AS target
+USING temp_earthquake_staging AS source
+ON target.earthquake_id = source.earthquake_id
+WHEN NOT MATCHED THEN
+  INSERT (earthquake_id, position, depth, magnitude, time, alert, significance)
+  VALUES (source.earthquake_id, source.position, source.depth, source.magnitude, source.time, source.alert, source.significance);
