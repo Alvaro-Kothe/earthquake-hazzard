6d0fe20a7d08752ceb36ce9562fe69cf21338669
Alvaro-Kothe <kothe65@gmail.com>
2025-01-27 21:25:20 -0300

    style(terraform): format variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
 variable "project" {
-  type    = string
-  default = "my-project"
+  type        = string
+  default     = "my-project"
   description = "Project name"
 }
 
 variable "location" {
-  type    = string
-  default = "US"
+  type        = string
+  default     = "US"
   description = "Location"
 }
 
 variable "region" {
-  type    = string
-  default = "us-central1"
+  type        = string
+  default     = "us-central1"
   description = "Project region"
 }
 
 variable "bucket_prefix" {
-  type    = string
-  default = "earthquake-data"
+  type        = string
+  default     = "earthquake-data"
   description = "Bucket prefix"
 }
 
